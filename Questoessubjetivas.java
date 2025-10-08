d8b15f8713473b3f7b77a75b35273d4ed4155a95
Natâny Dias <natanydias@gmail.com>
2025-10-07 21:07:13 -0300

    Atividades Slides Aula 07 - Agregação e Composição
    Questões de 1 a 4.

---
+++ Questoessubjetivas.java
@@ -0,0 +1,26 @@
+//1) Cite quatro exemplos diferentes que podem ser considerados
+//como agregação ou composição (dois de cada).
+//RESPOSTA: 
+//Agregação:
+//Professor e Turma → uma Turma pode existir sem professor definido.
+//Carro e Passageiro → o passageiro pode sair do carro e continuar existindo.
+//Composição:
+//Casa e Cômodo → um cômodo não pode existir sem a casa.
+//Computador e Placa Mãe → a placa mãe não pode existir sem o computador.
+
+//2) O que acontece se uma classe for programada como final? E se
+//um atributo for “setado” como final?
+//RESPOSTA:
+//Se uma classe for programada como final, ela não pode ser estendida (herdada) por outras classes.
+//Se um atributo for "setado" como final, ele não pode ser modificado após sua inicialização, ou seja, 
+//seu valor permanece constante durante a execução do programa
+
+//3) Qual a vantagem de programar um método estático?
+//RESPOSTA:
+//A vantagem é que ele pode ser chamado sem a necessidade
+//de criar um objeto. Isso é útil para utilitários ou métodos de cálculo.
+
+//4) Pesquise e cite mais dois métodos que fazem parte da classe Object.
+//RESPOSTA:
+//clone() → Cria uma cópia do objeto.
+//finalize() → Executado antes do objeto ser destruído pelo coletor de lixo (GC).
